72c69dc364948d386cfd93d9ec41029c342bd584
ratul.sikder <ratulsikder104@gmail.com>
2023-02-02 14:49:41 +0600

    header init

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import logo from "../../assets/images/logo.svg" 
 
 const Header = () => {
-    return <div>home</div>;
+    return (
+        <div className="main-header">
+            <div className="custom-container">
+                <a href="/">
+                    <img src={logo} alt="main-logo" />
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default Header;
